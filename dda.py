1741853f078005b116408ad1338120c3f605e64e
dhityl <rupinjnavaly@gmail.com>
2025-05-26 18:44:14 +0545

    fix step selection conditiong

--- dda.py
+++ dda.py
@@ -3,7 +3,7 @@ import sys
 
 pygame.init()
 
-WIDTH, HEIGHT = 800, 600
+WIDTH, HEIGHT = 1280, 1024
 WHITE = (225, 225, 225)
 BLACK = (0, 0, 0)
 
@@ -16,9 +16,9 @@ def draw_line_dda(x1,y1,x2,y2):
     dy = y2 - y1
 
     if abs(dx) > abs(dy):
-        step = dx
+        step = abs(dx)
     else:
-        step = dy
+        step = abs(dy)
         
     xinc = dx/step
     yinc = dy/step
@@ -26,7 +26,7 @@ def draw_line_dda(x1,y1,x2,y2):
     x, y  = x1, y1
     screen.set_at((round(x), round(y)), WHITE)
 
-    while x != x2 or y!= y2:
+    while x!=x2 or y!=y2:
         x += xinc
         y += yinc
         screen.set_at((round(x), round(y)), WHITE)
@@ -41,7 +41,7 @@ def main():
 
         screen.fill(BLACK)
 
-        draw_line_dda(20, 20, 20 , 580)
+        draw_line_dda(200, 800, 1000 , 400) # Call with coordinates for the line to draw
         pygame.display.flip()
 
 if __name__ == "__main__":
